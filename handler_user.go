438026d5b7702251f1e31c09385174e519639a90
SafariBallScrapyard <t_giaquinto@yahoo.com>
2025-11-20 22:28:11 -0500

    2: user registrations and tracking

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/SafariBallScrapyard/gator/internal/database"
+	"github.com/google/uuid"
 )
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+	if err != nil {
+		return fmt.Errorf("could not create current user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("could not set current user: %w", err)
+	}
+
+	fmt.Println("User registered successfully.")
+	printUser(user)
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -10,11 +41,36 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
 	if err != nil {
-		return fmt.Errorf("current user could not set: %w", err)
+		return fmt.Errorf("could not find user: %s", name)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("could not set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully.")
 	return nil
+}
+
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not list users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %s\n", user)
+	}
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
